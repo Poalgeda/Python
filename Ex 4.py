1e66b474f32045b57c3676d9a095c0497cbe658a
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:54:32 +0200

    Delete Ex 4.py

--- Ex 4.py
+++
@@ -1,8 +0,0 @@
-#A partir del código anterior, realiza una versión para números con decimales
-num1=float(input("Introduce un numero para la suma  "))
-num2=float(input("Introduce un segundo numero para la suma  "))
-
-total=num1+num2
-
-print ("El resultado de la suma es:", total)
-input()
