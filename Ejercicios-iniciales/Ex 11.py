8b8c612da179d8223fdc46e66b13bc3f97acb956
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:53:17 +0200

    Create Ex 11.py

---
+++ Ejercicios-iniciales/Ex 11.py
@@ -0,0 +1,8 @@
+#Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro.
+
+var1=int(input("Introduce la variable que se usará para determinar las medidas del cuadrado:   "))
+p=int(var1)*4
+a=int(var1)*int(var1)
+print("El perímetro del cuadrado es: ", p)
+print("El área del cuadrado es: ", a)
+input()
