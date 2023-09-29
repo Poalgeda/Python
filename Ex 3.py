a97bf4f6e5772b8d63380522ccd836fea7be5678
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:54:29 +0200

    Delete Ex 3.py

--- Ex 3.py
+++
@@ -1,6 +0,0 @@
-#Programa que pida dos números enteros y realice la suma correspondiente
-num1=int(input ("Introduce un numero para la suma- "))
-num2=int(input ("Introduce un segundo numero para la suma- "))
-total=num1+num2
-print ("El resultado es:", total)
-input()
