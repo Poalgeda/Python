9aa24cf7ac003284a949e7777d68204217f1c37c
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:53:37 +0200

    Create Ex 2.py

---
+++ Ejercicios-iniciales/Ex 2.py
@@ -0,0 +1,8 @@
+#Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla en el siguiente orden y formato: número entero, texto y número decvar1=imal.
+var1=int (input ("introduce un numero entero"))
+var2=input ("introduce una letra")
+var3=input ("introduce un numero decimal")
+print ("El valor introducido es un ", var1)
+print ("El valor introducido es la letra ", var2)
+print("El valor introducido es el número decimal ", var3)
+input ()
