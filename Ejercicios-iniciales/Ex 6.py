9bdd30467d8aada2bbb162a971ccbf85a3af158b
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:53:47 +0200

    Create Ex 6.py

---
+++ Ejercicios-iniciales/Ex 6.py
@@ -0,0 +1,12 @@
+# A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
+
+word1= input("Introduce la primera palabra: ")
+word2= input("Introduce la segunda palabra: ")
+word3= input("Introduce la tercera palabra: ")
+word4= input("Introduce la cuarta palabra: ")
+word5= input("Introduce la ultima palabra: ")
+
+print("Las frases resultantes son:",word1,word2,word3,word4,word5)
+print("                                         "  ,word5,word4,word3,word2,word1)
+input()
+
