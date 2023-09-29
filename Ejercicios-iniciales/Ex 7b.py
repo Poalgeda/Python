67dd3b55258aad6a0bb9e2d89b53657d677b1095
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:53:51 +0200

    Create Ex 7b.py

---
+++ Ejercicios-iniciales/Ex 7b.py
@@ -0,0 +1,22 @@
+#Introduce un numero de tres cifras para saber la suma de las mismas.
+
+num=input ("Introduce una numero de tres cifras  ")
+num1=num [0]
+num2=num[1]
+num3=num[2]
+suma=int (num1)+int(num2)+int(num3)
+print ("La suma de las tres cifras es:  ", suma)
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
