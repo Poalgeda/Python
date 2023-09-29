b7c9aed1241fe28f28a054a864760adebf84249e
Poalgeda <pol.beltran@campus.xaloc.org>
2023-09-29 09:54:44 +0200

    Delete Ex 8.py

--- Ex 8.py
+++
@@ -1,9 +0,0 @@
-#programa que pida un número de horas y muestre por pantalla los minutos y segundos
-
-num1=int(input("Introduce un numero en horas: "))
-
-minutos=int(num1)*60
-seg=int(num1)*3600
-
-print("Este numero equivale a", minutos, "minutos, y a",seg, "segundos")
-input()
