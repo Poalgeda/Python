16d57df6fe8fb5bf69a97753205b830104293298
Poalgeda <pol.beltran@campus.xaloc.org>
2023-10-03 10:44:25 +0200

    Update Ex 21.py

--- Ejercicios-condicionales/Ex 21.py
+++ Ejercicios-condicionales/Ex 21.py
@@ -7,11 +7,14 @@ raiz=math.sqrt(b**2-4*a*c)
 
 if raiz<0:
     print("Como la raiz es negativa no tiene un resultado posible")
+    input()
 elif raiz>0:
     x1=(-b+raiz)/(2*a)
-x2= (-b-raiz)/(2*a)
-print("La raiz es positiva: x1=",float(x1)," x2=", float(x2),".")
+    x2= (-b-raiz)/(2*a)
+    print("La raiz es positiva: x1=",float(x1)," x2=", float(x2),".")
+    input()
+else:
+    x= -b/(2*a)
+    print("Como la raiz es igual a 0, solo hay un resultado posible, x=", float(x))
 
-elif raiz==0: 
-x= -b/(2*a)
-print("Como la raiz es igual a 0, solo hay un resultado posible, x=", float(x))
+input()
