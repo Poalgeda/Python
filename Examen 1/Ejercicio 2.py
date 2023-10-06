bac4f38a9df956c8ae047e179b7a9cf24fbc4824
Poalgeda <pol.beltran@campus.xaloc.org>
2023-10-06 09:53:26 +0200

    Create Ejercicio 2.py

---
+++ Examen 1/Ejercicio 2.py
@@ -0,0 +1,9 @@
+#Ejercicio 2
+
+var1=float(input("Introduce el primer número: "))
+var2=float(input("Introduce el segundo número: "))
+var_total=var1+var2
+div=var_total/3
+div_total=round(div,3)
+print("El resultado de sumar", var1,"y",var2,"es:", var_total)
+print("El resultado de dividir ",var_total,"entre 3 es: ",div_total)
