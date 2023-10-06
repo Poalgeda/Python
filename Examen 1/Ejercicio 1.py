eca5e74ad9190dfb3e20e628fb83a31538c01741
Poalgeda <pol.beltran@campus.xaloc.org>
2023-10-06 09:53:28 +0200

    Create Ejercicio 1.py

---
+++ Examen 1/Ejercicio 1.py
@@ -0,0 +1,7 @@
+#Ejercicio 1
+
+
+var1=float(input("Introduce el primer número: "))
+var2=float(input("Introduce el segundo número: "))
+var_total=var1+var2
+print("El resultado de sumar", var1,"y",var2,"es:", var_total)
