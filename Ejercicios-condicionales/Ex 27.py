316d6235701ebd263fff85df3a3e5aacc7beb684
Poalgeda <pol.beltran@campus.xaloc.org>
2023-10-17 10:45:08 +0200

    Update Ex 27.py

--- Ejercicios-condicionales/Ex 27.py
+++ Ejercicios-condicionales/Ex 27.py
@@ -1,1 +1,4 @@
 #Mejora el programa anterior para controlar que el valor introducido es una letra y en caso de introducir un número, aparezca un aviso por pantalla
+
+var1=input("Introduce una letra, mayúscula o minúscula:  ")
+
