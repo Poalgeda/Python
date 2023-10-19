4f34d76f253d19fcf1e8dc22a636eb8241ce0862
Poalgeda <pol.beltran@campus.xaloc.org>
2023-10-19 09:53:33 +0200

    Update Ex 27.py

--- Ejercicios-condicionales/Ex 27.py
+++ Ejercicios-condicionales/Ex 27.py
@@ -1,4 +1,16 @@
 #Mejora el programa anterior para controlar que el valor introducido es una letra y en caso de introducir un número, aparezca un aviso por pantalla
 
-var1=input("Introduce una letra, mayúscula o minúscula:  ")
+var1=input("Introduce una letra:  ")
 
+numero=var1.isdigit()
+if numero is True:
+    print("El valor introducido es un numero.")
+elif numero is False:
+    if 'A' <= var1 <= 'Z': 
+            mayuscula = True
+    else:
+            mayuscula = False
+    if mayuscula:
+            print("La letra ingresada está en mayúscula.") 
+    else:
+            print("La letra ingresada está en minúscula.")
