867a3286b8b5a1038d14d0a13b74caf94cd57c58
Poalgeda <pol.beltran@campus.xaloc.org>
2023-10-24 10:08:04 +0200

    Create Ex 33.py

---
+++ Ejercicios-condicionales/Ex 33.py
@@ -0,0 +1,9 @@
+#Programa un código que permita contar el número de vocales de la siguiente frase: No 
+#hay mal que dure cien años
+stc="No hay mal que dure cien años"
+a=stc.count("a")
+e=stc.count("e")
+i=stc.count("i")
+o=stc.count("o")
+u=stc.count("u")
+print(f"El número de a es {a} el número de e es {e} el número de i es {i} el número de os es {o} y el número de u es {u}")
