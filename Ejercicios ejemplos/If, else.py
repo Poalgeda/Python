5696157ad0babe65d3b23fd8aad07638475bdcee
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-10 09:23:09 +0100

    Create If, else.py

---
+++ Ejercicios ejemplos/If, else.py
@@ -0,0 +1,12 @@
+Num=2
+for x in range (num):
+
+var1=input("Introducir contraseña:      ")
+
+if var1==1:
+    print ("Contraseña correcta, permiso concedido")
+
+else:
+    print("Contraseña incorrecta, permiso no concedido")
+
+input()
