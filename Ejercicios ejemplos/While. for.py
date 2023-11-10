f47763c24f2432d7dd0cd430c0f3477ad632af82
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-10 09:23:17 +0100

    Create While. for.py

---
+++ Ejercicios ejemplos/While. for.py
@@ -0,0 +1,32 @@
+#Imprimir x veces el contador, se irá incrementando.
+
+#----for contador in range (0,10):
+#----    print(contador)
+
+#Mismo ejemplo que el anterior pero creando series de 2,3,4...
+
+#----for contador in range(0,10,2):
+#----    print(contador)
+
+#Imprimir frase x veces.
+
+#----for frase in range (0,10):
+#----    print("Hola")
+
+#Ejemplo recorrer string con un rango determinado.
+
+#----password="bon dia"
+#----longitud=len(password)
+#----for contador in range(0,longitud):
+#----    print(password[contador])
+
+#EJemplo recorrer string establecido los 'topes' a través de variables.
+#¿Qué ocurre si ponemos un tope superior a la longitud de la variable?
+
+
+
+#Ejemplo recorrer un string. for variable in string.
+
+password="buenosdias"
+for var in password:
+    print(var)
