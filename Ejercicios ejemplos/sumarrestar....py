698e6e433fb01b51a6f9006aef986d10b8d239a4
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-10 09:23:19 +0100

    Create sumarrestar....py

---
+++ Ejercicios ejemplos/sumarrestar....py
@@ -0,0 +1,25 @@
+print ("suma-------------------------------")
+var1=float (input("introduce un valor: "))
+var2=float (input("introduce otro valor: "))
+total=var1+var2
+print ("el resultado es: ", total)
+
+print ("resta-------------------------------")
+var1=float (input("introduce un valor: "))
+var2=float (input("introduce otro valor: "))
+total=var1-var2
+print ("el resultado es: ", total)
+
+print ("multiplicación-------------------------------")
+var1=float (input("introduce un valor: "))
+var2=float (input("introduce otro valor: "))
+total=var1*var2
+print ("el resultado es: ", total)
+
+print ("división-------------------------------")
+var1=float (input("introduce un valor: "))
+var2=float (input("introduce otro valor: "))
+total=var1/var2
+print ("el resultado es: " , total)
+
+
