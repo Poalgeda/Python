af9c220249503a269e1613ae889e40711bf17b20
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-16 09:52:43 +0100

    Update Ex 43.py

--- Ejercicios-bucles/Ex 43.py
+++ Ejercicios-bucles/Ex 43.py
@@ -1,2 +1,4 @@
 #Realiza un programa que recorra con un for una palabra introducida por teclado y se imprima por pantalla cada letra
 
+palabra=input("Introduce una palabra")
+posiciones=len(palabra)
