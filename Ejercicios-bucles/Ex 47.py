b9044257a92ed204f8a0a979bc7d3c13014604a0
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-17 09:53:03 +0100

    Create Ex 47.py

---
+++ Ejercicios-bucles/Ex 47.py
@@ -0,0 +1,4 @@
+# Realiza un programa donde el usuario introduzca por teclado 2 intervalos, por pantalla se debe mostrar el rango de números teniendo en cuenta que se a<b la secuencia será incremental y si a>b la secuencia en descenso. Respeta el formato de salida
+
+var1=int(input("Introduce el primer intervalo: "))
+var2=int(input("Introduce el segundo intervalo: "))
