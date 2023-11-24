8001af16ed8f8943c18e5a54b394a685bfaaf081
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-24 09:53:29 +0100

    Delete Ex 35.py

--- Ejercicios-bucles/Ex 35.py
+++
@@ -1,5 +0,0 @@
-#Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre.
-veces=int(input("¿Cuantas veces deseas repetir el mensaje?: "))
-for contador in range(0, veces):
-    print("Pol Beltran Castellà")
-    
