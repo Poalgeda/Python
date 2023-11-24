73928e9157d756828a8e1da6be37101007060b0c
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-24 09:53:05 +0100

    Create Ex 51.py

---
+++ Ejercicios_bucles_while/Ex 51.py
@@ -0,0 +1,8 @@
+#A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el número de veces que desea que repita la frase Buenos días. Con While.
+
+cont=int(input("¿Cuantas veces quieres repetir el programa? -> "))
+
+var1=0
+while var1!=cont:
+    print("buenos dias")
+    var1+=1
