98e2af30a21fc09d44d577c428293a7248baad6e
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-24 09:52:48 +0100

    Update Ex 49.py

--- Ejercicios-bucles/Ex 49.py
+++ Ejercicios-bucles/Ex 49.py
@@ -6,6 +6,6 @@ for cont in range(0, len(palabra)):
     letra=input("Introduce una letra: ")
     pos_letra=palabra.find(letra) + 1
     if letra in palabra and len(letra)==1:
-        print(f"La letra {letra} existe y se encuantra en la posición {pos_letra}.")
+        print(f"La letra {letra} existe y se encuentra en la posición {pos_letra}.")
     else:
         print(f"La letra {letra} no existe.")
