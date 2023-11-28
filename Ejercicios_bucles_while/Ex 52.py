5982cc4abbd31c12a7c9fed8cb84af959ce1d4ba
Poalgeda <pol.beltran@campus.xaloc.org>
2023-11-28 10:44:37 +0100

    Update Ex 52.py

--- Ejercicios_bucles_while/Ex 52.py
+++ Ejercicios_bucles_while/Ex 52.py
@@ -1,11 +1,11 @@
 #Realiza un programa que sume dos números enteros por teclado y presente el resultado por pantalla. El programa preguntará si deseas o no repetir la operación. Con While 
 
-
-
 respuesta = "s"
-while respuesta != "n":
-    repuesta=int(input("¿Deseas repetir la operación? s/n: "))
+while respuesta == "s":
     var1=int(input("Introduce el primer numero: "))
     var2=int(input("Introduce el primer numero: "))
     suma=var1+var2
     print(f"El resultado de la suma es {suma}")
+    respuesta=input("¿Deseas repetir la operación? s/n: ")
+
+input("Fin del programa")
