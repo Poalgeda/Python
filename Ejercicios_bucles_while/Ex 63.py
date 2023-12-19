91e919935b4cbb867f54e1b7dc1ef5ecdd21809e
Poalgeda <pol.beltran@campus.xaloc.org>
2023-12-19 10:52:17 +0100

    Update Ex 63.py

--- Ejercicios_bucles_while/Ex 63.py
+++ Ejercicios_bucles_while/Ex 63.py
@@ -1,4 +1,33 @@
 #Realiza un programa que permita tirar 100 veces un dado y nos presente por pantalla el número de veces que se repite cada número.
 
 import random
-num=random.randint(1, 6)
+tiros=0
+uno=0
+dos=0
+tres=0
+cuatro=0
+cinco=0
+seis=0
+while tiros !=100:
+    tiros +=1
+    num=random.randint(1, 6)
+    if num == 1:
+        uno +=1
+    elif num ==2:
+        dos +=1
+    elif num==3:
+        tres+=1
+    elif num==4:
+        cuatro +=1
+    elif num == 5:
+        cinco +=1
+    elif num == 6:
+        seis +=1
+
+
+print(f"Unos: {uno}")
+print(f"Doses: {dos}")
+print(f"Treses: {tres}")
+print(f"Cuatros: {cuatro}")
+print(f"Cincos: {cinco}")
+print(f"Seises: {seis}")
