6f873447a78298323251e2880a169e67f32da95e
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-09 10:44:00 +0100

    Update Fase1.py

---
+++ proyecto_7_5/Fase1.py
@@ -0,0 +1,8 @@
+#Programa  el  código  que  gestione  una  partida.  El  programa  ofrece  una  carta,  el  usuario  puede  o  noaceptar. La puntuación se acumula según el número de carta que aparece al azar.
+
+import random
+respuesta1="s"
+respuesta2="s"
+while respuesta1=="s":
+    while respuesta2=="s":
+        
