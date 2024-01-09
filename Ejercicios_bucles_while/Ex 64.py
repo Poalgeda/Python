f33df1ddc30bdceb63cec2e1f09b66dea94efe6a
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-09 10:11:12 +0100

    Update Ex 64.py

--- Ejercicios_bucles_while/Ex 64.py
+++ Ejercicios_bucles_while/Ex 64.py
@@ -5,20 +5,34 @@
 #d) total de números negativos 
 #e) total de ceros 
 #f) total de la suma de todos los números introducidos
-
+import time
 ceros=0
 pares=0
 impares=0
 positivos=0
 negativos=0
 Suma=0
-
+var1=0
+var1=int(input("Introduce un numero: "))
 while var1 != -99:
-    var1=int(input("Introduce un numero: "))
+    resto= var1 % 2
     if var1 == 0:
         ceros +=1
-    resto= % 2
-    if resto==0 and var1!=0:
+    elif resto==0:
         pares+=1
-    else:
+    elif resto > 0 or resto < 0:
         impares =+1
+    if var1 > 0:
+        positivos +=1
+    elif var1 < 0:
+        negativos +=1
+    Suma +=var1
+    var1=int(input("Introduce un numero: "))
+
+print("*****RESUMEN*****")
+print(f"Ceros: {ceros}")
+print(f"Numeros pares: {pares}")
+print(f"Numeros impares: {impares}")
+print(f"Numeros positivos: {positivos}")
+print(f"Numeros negativos: {negativos}")
+print(f"Suma total: {Suma}")
