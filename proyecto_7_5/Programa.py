7cfca342de106988c4ebe39110100777cc454f93
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-18 09:40:25 +0100

    Update Programa.py

--- proyecto_7_5/Programa.py
+++ proyecto_7_5/Programa.py
@@ -1,1 +1,10 @@
-import tinker  
+import tkinter as tk
+from tkinter import ttk  
+
+
+
+ventana = tk.Tk()
+ventana.title("Conversor de temperatura")
+ventana.config(width=400, height=300)
+etiqueta_temp_celsius = ttk.Label(text="Temperatura en ºC:")
+etiqueta_temp_celsius.place(x=20, y=20)
