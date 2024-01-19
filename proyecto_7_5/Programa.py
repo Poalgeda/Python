fc4c83c093149099e4be4e28ed0f5042df5103e2
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-19 09:48:59 +0100

    Update Programa.py

--- proyecto_7_5/Programa.py
+++ proyecto_7_5/Programa.py
@@ -1,16 +1,31 @@
 import tkinter as tk
-from tkinter import ttk  
-#Ahora vamos a dar un título y un tamaño a la ventana, vía los métodos title() y config()
+from tkinter import ttk
+def convertir_temp():
+    temp_celsius = float(caja_temp_celsius.get())
+    temp_kelvin = temp_celsius + 273.15
+    temp_fahrenheit = temp_celsius*1.8 + 32
+    etiqueta_temp_kelvin.config(text=f"Temperatura en K: {temp_kelvin}")
+    etiqueta_temp_fahrenheit.config(
+        text=f"Temperatura en ºF: {temp_fahrenheit}")
+    
 ventana = tk.Tk()
+
 ventana.title("Conversor de temperatura")
 ventana.config(width=400, height=300)
-ventana.mainloop
-#Empecemos por mostrar un mensaje en la ventana que indique al usuario que debe ingresar la temperatura en grados Celsius,
-# para lo cual podemos usar un control o widget llamado etiqueta. La etiqueta está representada en Tk por la clase ttk.Label.
-ventana = tk.Tk()
-ventana.title("Conversor de temperatura")
-ventana.config(width=400, height=300)
+
 etiqueta_temp_celsius = ttk.Label(text="Temperatura en ºC:")
 etiqueta_temp_celsius.place(x=20, y=20)
 
-#LINK: https://recursospython.com/guias-y-manuales/introduccion-a-tkinter/
+caja_temp_celsius = ttk.Entry()
+caja_temp_celsius.place(x=140, y=20, width=60)
+
+boton_convertir = ttk.Button(text="Convertir", command=convertir_temp)
+boton_convertir.place(x=20, y=60)
+
+etiqueta_temp_kelvin = ttk.Label(text="Temperatura en K: n/a")
+etiqueta_temp_kelvin.place(x=20, y=120)
+
+etiqueta_temp_fahrenheit = ttk.Label(text="Temperatura en ºF: n/a")
+etiqueta_temp_fahrenheit.place(x=20, y=160)
+
+ventana.mainloop()
