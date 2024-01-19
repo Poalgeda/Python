a49aa4f6506ab36809b6cf0cf71389dfb32863df
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-19 21:09:24 +0100

    Update programa2.py

--- MIS PROYECTOS/programa2.py
+++ MIS PROYECTOS/programa2.py
@@ -1,10 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-
-
-
 ventana = tk.Tk()
 
 ventana.title("Conversor de temperatura")
-ventana.config(width=400, height=300)
+ventana.config(width=1000, height=1000)
+ventana.mainloop()
 
