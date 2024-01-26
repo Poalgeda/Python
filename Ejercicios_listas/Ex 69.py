d6f8fd294ae99429ec53ffbb0628521dc0a4747d
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-26 09:52:16 +0100

    Update Ex 69.py

--- Ejercicios_listas/Ex 69.py
+++ Ejercicios_listas/Ex 69.py
@@ -1,10 +1,9 @@
 #69. Realiza un programa que permita introducir una cantidad exacta de números, cada número se irá almacenando en una lista. El programa debe finalizar presentando por pantalla los números ordenados de menor a mayor.
 
-
-total=""
+lista=[]
 num=int(input("Cuantos numeros quieres introducir: "))
 for cont in range(0,num):
-    var1=input("Introduce un numero: ")
+    var1 =input("Introduce un numero: ")
     lista += sorted(var1.split(","))
 print(lista)
 
