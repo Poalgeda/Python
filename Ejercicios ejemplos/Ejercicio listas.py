e68609aa29221f027d901655e4501f51a2df914c
Poalgeda <pol.beltran@campus.xaloc.org>
2024-01-30 10:44:43 +0100

    Create Ejercicio listas.py

---
+++ Ejercicios ejemplos/Ejercicio listas.py
@@ -0,0 +1,16 @@
+
+listanom=[]
+listaedad=[]
+listapes=[]
+listaaltu=[]
+
+respuesta="s"
+
+while respuesta=="s":
+    nom=input("Introduce el nombre una persona: ")
+    edad=int(input("Introduce la edad de la persona en años: "))
+    peso=float(input("Introduce el peso de la persona en kilos: "))
+    altura=float(input("Introduce la altura en metros: "))
+
+
+
