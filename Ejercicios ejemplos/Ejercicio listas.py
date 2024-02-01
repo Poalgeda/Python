93eab9650e31181d77094121bfa934482e3c27c7
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-01 09:51:55 +0100

    Update Ejercicio listas.py

--- Ejercicios ejemplos/Ejercicio listas.py
+++ Ejercicios ejemplos/Ejercicio listas.py
@@ -9,13 +9,13 @@ respuesta="s"
 while respuesta=="s":
     nom=input("Introduce el nombre una persona: ")
     edad=int(input("Introduce la edad de la persona en años: "))
-    peso=float(input("Introduce el peso de la persona en kilos: "))
-    altura=float(input("Introduce la altura en metros: "))
+    #peso=float(input("Introduce el peso de la persona en kilos: "))
+    #altura=float(input("Introduce la altura en metros: "))
 
     listanom.append(nom)
     listaedad.append(edad)
-    listapes.append(peso)
-    listaaltu.append(altura)
+    #listapes.append(peso)
+    #listaaltu.append(altura)
 
     respuesta=input("Deseas añadir otra persona? s/n ")
 
@@ -24,6 +24,7 @@ respuesta=input("Deseas buscar a una persona? s/n ")
 while respuesta=="s":
     buscar=input("Introduce el nombre de la persona: ")
     encontrado=listanom.count(buscar)
+
     if encontrado>0:
         indice=listanom.index(buscar)
         print(f"la edad de {listanom[indice]} es ",listaedad[indice])
