ca689a566d78bdcb90d91f03ae46a7680dbfcdfd
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-15 09:51:51 +0100

    Update Ex 74.py

--- Ejercicios_listas/Ex 74.py
+++ Ejercicios_listas/Ex 74.py
@@ -1,16 +1,18 @@
 #A partir del programa anterior, haz que se visualicen tanto las palabras que se repiten o no de entre las 2 listas.
-
+input()
 lista1=['casa','mesa','sal','sol','agua']
 lista2=['casa','luz','tres','tren','sol','pan']
 repetidos=[]
 no_repetidos=[]
-num1=len(lista2)
 #Que vaya valor por valor comprovando si se repiten y se vayan sumando a sus correspondientes listas.
-for cont in lista2:
-    if lista2[0] == cont:
-        repetidos += lista2[0].split(",")
+for cont in lista1:
+    if cont in lista2:
+        repetidos += cont.split(",")
+        i = lista2.index(cont)
+        lista2.pop(i)
     else:
         no_repetidos += lista2[0].split(",")
+        no_repetidos += cont.split(",")
         lista2.pop(0)
 #visualizar por pantalla.
 print(repetidos)
