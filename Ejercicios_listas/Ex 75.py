6a7bf704c281cf1257cf371460d9ec45a4cbe762
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-15 09:51:53 +0100

    Create Ex 75.py

---
+++ Ejercicios_listas/Ex 75.py
@@ -0,0 +1,14 @@
+#Crea una lista con el siguiente nombre lista1 y su contenido: a,b,D,x,r,X,3,h,w,2,i. Presenta por pantalla los siguientes resultados: 
+#a. Cantidad total de valores 
+#b. Cantidad de números 
+#c. Cantidad de letras 
+#d. Cantidad de mayúsculas 
+#e. Suma de los valores numéricos
+
+lista1=["a","b","D","x","r","X",3,"h","w",2,"i"]
+letras=0
+mayus=0
+suma_num=0
+valores=len(lista1)
+range=is
+print(f"Número de valores: {valores}")
