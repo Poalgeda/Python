b9b96f9fff4f07183bfcc18d351e7355b0c03657
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-22 09:53:23 +0100

    Update Ex 75.py

--- Ejercicios_listas/Ex 75.py
+++ Ejercicios_listas/Ex 75.py
@@ -5,11 +5,25 @@
 #d. Cantidad de mayúsculas 
 #e. Suma de los valores numéricos
 
-lista1=["a","b","D","x","r","X",3,"h","w",2,"i"]
+lista1 = ['a', 'b', 'D', 'x', 'r', 'X', '3', 'h', 'w', '2', 'i']
+
+total = len(lista1)
+numeros = 0
+mayusculas = 0
 letras=0
-nums=0
-mayus=0
-suma_num=0
-valores=len(lista1)
-print(f"Número de valores: {valores}")
-lista1.count(nums)
+suma_nums = 0
+for elemento in lista1:
+    if elemento.isdigit():
+        numeros += 1
+    if elemento.isalpha():
+        letras += 1
+    if elemento.isdigit():
+        suma_nums += int(elemento)
+    if elemento.isupper():
+        mayusculas += 1
+
+print("Número de valores:", total)
+print("Cantidad de números:", numeros)
+print("Cantidad de letras:", letras)
+print("Cantidad de mayúsculas:", mayusculas)
+print("Suma total de números:", suma_nums)
