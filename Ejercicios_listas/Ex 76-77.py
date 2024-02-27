a33fc61dddb261da5ee97117d2e3ddffffa27a2f
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-27 10:45:33 +0100

    Update Ex 76-77.py

--- Ejercicios_listas/Ex 76-77.py
+++ Ejercicios_listas/Ex 76-77.py
@@ -9,16 +9,15 @@ lista_numeros=[]
 lista_letras=[]
 var1="a"
 
-#Que adjudique una lista por si es numero o letra, las mayusculas tambien els añado en una lista aparte para detectar cuales son 
-#Y mas tarde convertirlas en minusculas, para ordenar la lista y luego convertirlas nuevamente en mayúsculas.
+#Que adjudique una lista por si es numero o letra, las mayusculas tambien las añado en una lista aparte para detectar cuales son 
+#Y mas tarde insertarlas, con ayuda de un for, en la posicion que les toca.
 for elemento in lista1:
     if elemento.isdigit():
         lista_numeros+=elemento.split(",")
-    if elemento.isalpha():
+    if elemento.isalpha() and elemento.islower():
         lista_letras+=elemento.split(",")
-    if elemento.isupper():
+    if elemento.isupper() and elemento.isalpha():
         mayusculas += elemento.split(",")
-
 #He añadido un while para que se tenga que poner un 1 o un 2 y si no se cumple el programa te avisa, en vez de que salga un error en la consola.
 while var1=="a":
     asc_desc=int(input("Introduce 1 para visualizar en orden ascendente o 2 descendente: "))
@@ -26,16 +25,14 @@ while var1=="a":
         print("Por favor, introduce el número 1 o 2.")
         var1="a"
     elif asc_desc == 1:
-        #localizar las mayusculas
-        i_1=lista_letras.index("X")
-        i_2=lista_letras.index("D")
-        i_1=str(i_1).lower()
-        i_2=str(i_2).lower()
         lista_numeros.sort()
         lista_letras.sort()
+        for 
+        #Como habrán ahora dos letras repetidas con el nombre de 'x', elegimos la segunda y la convertimos en mayuscula, reciclando variables.
         
         print(lista_numeros)
         print(lista_letras)
+        
     elif asc_desc == 2:
         lista_numeros.sort(reverse=True)
         lista_letras.sort(reverse=True)
