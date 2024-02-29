29aadacbfc151f922bc747856c572a95f2b9ab6c
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-29 09:53:36 +0100

    Create Ex 80.py

---
+++ Ejercicios_listas/Ex 80.py
@@ -0,0 +1,9 @@
+#Utilizando listas, crea un programa que te permita determinar si un número es decimal o no.
+#con esta variable, he hecho un while "infinito"
+Ex80="ñ"
+while Ex80=="ñ":
+    var1=input("Introduce un valor: ")
+    if var1.isdecimal():
+        print("Es un numero con decimales")
+    else:
+        print("No es un numero con decimales")
