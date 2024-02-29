42d2f98d85c8d78dd65f0f3c48fa6e20ca18be7c
Poalgeda <pol.beltran@campus.xaloc.org>
2024-02-29 09:53:40 +0100

    Create Ex 79.py

---
+++ Ejercicios_listas/Ex 79.py
@@ -0,0 +1,7 @@
+#A través de la variable definida var1="a,b,1,r,sal,34,mar”. Pasa estos valores a una lista y haz que presente por pantalla cantidad de valores almacenada. Utiliza el método que corresponde. Máximo 3 líneas de código.
+
+#--------------------------------****Codigo****----------------------------------------
+var1 = "a,b,1,r,sal,34,mar"
+lista_var1 = var1.split(',')
+print(len(lista_var1))
+#----------------------------------****Fin****-----------------------------------------
