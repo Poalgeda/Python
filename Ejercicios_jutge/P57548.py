93b14bff997d9a250ba3bd9d8adfaefe4ddf1c90
Poalgeda <pol.beltran@campus.xaloc.org>
2024-03-05 10:46:10 +0100

    Update P57548.py

--- Ejercicios_jutge/P57548.py
+++ Ejercicios_jutge/P57548.py
@@ -1,17 +1,8 @@
 var=input("")
 lista=var.split()
 if len(lista) == 3:
-    print(int(lista[0])+int(lista[1])+int(lista[2]))
-elif len(lista) == 2:
+    print(int(lista[0])+int(lista[1]))
+else:
     var1=input("")
     lista+=var1.split()
-    print(int(lista[0])+int(lista[1])+int(lista[3]))
-elif len(lista) == 1:
-    var1=input("")
-    lista+=var1.split()
-    if len(lista) == 2:
-        var1=input("")
-        lista+=var1.split()
-        print(int(lista[0])+int(lista[1])+int(lista[2]))
-    else:
-        print(int(lista[0])+int(lista[1])+int(lista[2]))
+    print(int(lista[0])+int(lista[1]))
