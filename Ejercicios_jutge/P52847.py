71d5f9f99b543ef100edc5e48e86f2770a8431d2
Poalgeda <pol.beltran@campus.xaloc.org>
2024-03-08 09:02:54 +0100

    Create P52847.py

---
+++ Ejercicios_jutge/P52847.py
@@ -0,0 +1,8 @@
+var=input("")
+lista=var.split()
+if int(lista[0]) > int(lista[1]) and int(lista[0]) > int(lista[2]):
+    print(lista[0])
+elif int(lista[1]) > int(lista[0]) and int(lista[1]) > int(lista[2]):
+    print(lista[1])
+elif int(lista[2]) > int(lista[0]) and int(lista[2]) > int(lista[1]):
+    print(lista[2])
