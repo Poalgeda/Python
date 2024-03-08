e58a228a755ad5ee2f09b6f173d58fc0dc4038f7
Poalgeda <pol.beltran@campus.xaloc.org>
2024-03-08 09:53:39 +0100

    Update P90615.py

--- Ejercicios_jutge/P90615.py
+++ Ejercicios_jutge/P90615.py
@@ -6,7 +6,17 @@ elif int(lista[1]) > int(lista[0]) and int(lista[1]) > int(lista[2]):
     print(lista[1])
 elif int(lista[2]) > int(lista[0]) and int(lista[2]) > int(lista[1]):
     print(lista[2])
-elif lista[0]==lista[1] and lista[0]==lista[2]:
+elif int(lista[0])==int(lista[1]) and int(lista[0])==int(lista[2]):
     print(lista[0])
-elif lista[0]==lista[1]:
+elif int(lista[0])==int(lista[1]) and int(lista[0])>int(lista[2]):
     print(lista[0])
+elif int(lista[0])==int(lista[1]) and int(lista[0])<int(lista[2]):
+    print(lista[2])
+elif int(lista[1])==int(lista[2]) and int(lista[1])<int(lista[0]):
+    print(lista[0])
+elif int(lista[1])==int(lista[2]) and int(lista[1])>int(lista[0]):
+    print(lista[1])
+elif int(lista[2])==int(lista[0]) and int(lista[0])>int(lista[1]):
+    print(lista[0])
+elif int(lista[2])==int(lista[0]) and int(lista[0])<int(lista[1]):
+    print(lista[1])
