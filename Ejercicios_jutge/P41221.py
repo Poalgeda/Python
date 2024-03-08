ba2bf716105e0a6fe561a76676ecfe6bfb96831d
Poalgeda <pol.beltran@campus.xaloc.org>
2024-03-08 09:02:48 +0100

    Update P41221.py

--- Ejercicios_jutge/P41221.py
+++ Ejercicios_jutge/P41221.py
@@ -5,7 +5,14 @@ if len(lista) == 3:
 elif len(lista) == 2:
     var1=input("")
     lista+=var1.split()
-    print(int(lista[0])+int(lista[1])+int(lista[3]))
+    if len(lista) == 2:
+        var1=input("")
+        lista+=var1.split()
+        print(int(lista[0])+int(lista[1])+int(lista[2]))
+    else:
+        var1=input("")
+        lista+=var1.split()
+        print(int(lista[0])+int(lista[1])+int(lista[2]))
 elif len(lista) == 1:
     var1=input("")
     lista+=var1.split()
