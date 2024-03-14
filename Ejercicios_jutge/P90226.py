7ac3966522f4531748ea30203707d6415180703a
Poalgeda <pol.beltran@campus.xaloc.org>
2024-03-14 09:43:41 +0100

    Update P90226.py

--- Ejercicios_jutge/P90226.py
+++ Ejercicios_jutge/P90226.py
@@ -1,1 +1,15 @@
 var=input("")
+
+lista_original=var.split(" ")
+lista_sort=var.split(" ")
+if lista_original[0] == lista_original[1]:
+    print(lista_original[0],"=",lista_original[1])
+else:
+    lista_sort.sort()
+    i=lista_original.index(lista_sort[0])
+
+    if i==0:
+        print(lista_original[0],"<",lista_original[1])
+    elif i==1:
+            print(lista_original[0],">",lista_original[1])
+            
